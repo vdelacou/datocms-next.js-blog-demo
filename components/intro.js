4e94ad16b36c37c78cfc54c218d6fb5cc74a3901
Vincent Delacourt <vincent.delacourt@joinhorizons.com>
2022-04-11 13:44:57 +0800

    change blog to Global Perspectives

--- components/intro.js
+++ components/intro.js
@@ -1,25 +1,11 @@
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+    <section className="flex flex-col items-center mt-16 mb-16 md:flex-row md:justify-between md:mb-12">
+      <h1 className="text-6xl font-bold leading-tight tracking-tighter md:text-8xl md:pr-8">
+      Global Perspectives
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://www.datocms.com/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          DatoCMS
-        </a>
-        .
+      <h4 className="mt-5 text-lg text-center md:text-left md:pl-8">
+        Unlock global opportunities
       </h4>
     </section>
   );
